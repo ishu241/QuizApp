cb5768764901d5f7ced745740aba0ea931c8876c
Abhishek Sagar <sagar.abhishek1996@gmail.com>
2017-09-07 01:33:55 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var correct = ["10", "1", "120", "594", "1000", "144"];
+
+
+$(".options div").on("click", function() {
+	var check = $(this).attr("class");
+	$("." + check).prop("checked", true);
+
+});
+
+$("#box").on("click", function() {
+	var score = 0;
+	var arr = [];
+	arr = document.querySelectorAll("input:checked + li");
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].textContent === correct[i]) {
+			score++;
+
+		}
+
+	}
+	$("#scoreboard").append("Your score is : " + score + "/6");
+	$("#scoreboard").css("display", "block");
+	 $('html, body').animate({scrollTop:$(document).height()}, 'slow');  //Scroll to the bottom of the page
+});
